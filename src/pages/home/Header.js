92dd6c34d5fbde97f6c6a8f517ea32777407b961
Yash Shah <ybshah02@gmail.com>
2024-07-30 12:09:56 -0500

    getting ready for deployment

--- src/pages/home/Header.js
+++ src/pages/home/Header.js
@@ -9,9 +9,9 @@ function Header() {
         <NavLink to="/"><img src="/assets/logo_wo_whitespace.png" className="logo"></img></NavLink>
         <ul>
           <li><NavLink to="/projects" className={({ isActive }) => isActive ? 'active' : ''}>Projects</NavLink></li>
-          <li><NavLink to="/blog" className={({ isActive }) => isActive ? 'active' : ''}>Blog</NavLink></li>
-          <li><NavLink to="/about-me" className={({ isActive }) => isActive ? 'active' : ''}>About me</NavLink></li>
-          <li><NavLink to="/contact-me" className={({ isActive }) => isActive ? 'active' : ''}>Contact Me</NavLink></li>
+          {/*<li><NavLink to="/blog" className={({ isActive }) => isActive ? 'active' : ''}>Blog</NavLink></li>*/}
+          {/*<li><NavLink to="/about-me" className={({ isActive }) => isActive ? 'active' : ''}>About me</NavLink></li>
+          <li><NavLink to="/contact-me" className={({ isActive }) => isActive ? 'active' : ''}>Contact Me</NavLink></li>*/}
         </ul>
         <div className="header-right">
         <div className="social-icons">
